f424b06c4f5422fafd6e89834e6bcb5cc6a6b3d3
TakatoYoshikawa <tenten0727@icloud.com>
2021-02-24 23:01:00 +0900

    step23 core_simple

---
+++ dezero/__init__.py
@@ -0,0 +1,9 @@
+from dezero.core_simple import Variable
+from dezero.core_simple import Function
+from dezero.core_simple import using_config
+from dezero.core_simple import no_grad
+from dezero.core_simple import as_array
+from dezero.core_simple import as_Variable
+from dezero.core_simple import setup_Variable
+
+setup_Variable()
