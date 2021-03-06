f3e12040da7b411c5279f02700631f8a499f6dcc
TakatoYoshikawa <tenten0727@icloud.com>
2021-03-06 18:46:40 +0900

    step32

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,9 +1,19 @@
-from dezero.core_simple import Variable
-from dezero.core_simple import Function
-from dezero.core_simple import using_config
-from dezero.core_simple import no_grad
-from dezero.core_simple import as_array
-from dezero.core_simple import as_Variable
-from dezero.core_simple import setup_Variable
+is_simple_core = False
 
+if is_simple_core:
+	from dezero.core_simple import Variable
+	from dezero.core_simple import Function
+	from dezero.core_simple import using_config
+	from dezero.core_simple import no_grad
+	from dezero.core_simple import as_array
+	from dezero.core_simple import as_Variable
+	from dezero.core_simple import setup_Variable
+else:
+	from dezero.core import Variable
+	from dezero.core import Function
+	from dezero.core import using_config
+	from dezero.core import no_grad
+	from dezero.core import as_array
+	from dezero.core import as_Variable
+	from dezero.core import setup_Variable
 setup_Variable()
